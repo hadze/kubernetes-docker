e6437aec29ae56c2e0b94760a3777dbed64ff1e4
Armin Hadzalic <hadzalic@live.de>
2020-06-29 22:53:43 +0200

    Add container with flask application

---
+++ flask/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+# list of images
+images = [
+    "https://iconarchive.com/show/my-seven-icons-by-itzikgur/Backup-IBM-Server-icon.html",
+    "https://iconarchive.com/show/oxygen-icons-by-oxygen-icons.org/Places-network-server-database-icon.html",
+    "https://iconarchive.com/show/windows-8-icons-by-icons8/Network-Server-icon.html",
+    "https://iconarchive.com/show/oxygen-icons-by-oxygen-icons.org/Mimetypes-application-x-smb-server-icon.html"
+]
+
+@app.route('/')
+def index():
+    url = random.choice(images)
+    return render_template('index.html', url=url)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
